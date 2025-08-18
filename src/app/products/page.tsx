f7f7a39f622a6952dabae3ef30cdae0134319f1e
Inna P <nonamer099@gmail.com>
2025-08-18 16:52:55 +0300

    link component

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,18 @@
+import Link from "next/link";
+
 export default function ProductList() {
+  const productId = 100;
   return (
     <>
       <h1>Products</h1>
-      <h2>Product1</h2>
-      <h2>Product2</h2>
-      <h2>Product3</h2>
+      <Link href="/products/1"> Product1</Link>
+      <Link href="/products/2">Product2</Link>
+      <Link href="/products/3" replace>
+        Product3
+      </Link>
+      <Link href={`/products/${productId}`}>Product{productId}</Link>
+
+      <Link href="/">Home</Link>
     </>
   );
 }
