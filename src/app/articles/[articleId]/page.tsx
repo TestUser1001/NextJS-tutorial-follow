3a86ee0ded734550427a1eae875fcc13d8b4ef58
Inna P <nonamer099@gmail.com>
2025-08-23 17:01:13 +0300

    params and search params

---
+++ src/app/articles/[articleId]/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Link from "next/link";
+import { use } from "react";
+
+export default function NewsArticle(
+  {
+    params,
+    searchParams,
+  }: {
+    params: Promise<{ articleId: string }>;
+    searchParams: Promise<{ lang?: "en" | "es" | "fr" }>;
+  } /**/
+) {
+  const { articleId } = use(params);
+  //{lang = "en"} default value = en
+  const { lang = "en" } = use(searchParams);
+  return (
+    <div>
+      <h1>News article {articleId}</h1>
+      <p>Reading in language {lang}</p>
+      <div>
+        <Link href={`/articles/${articleId}?lang=en`}>English</Link>
+      </div>
+      <div>
+        <Link href={`/articles/${articleId}?lang=es`}>Spanish</Link>
+      </div>
+      <div>
+        <Link href={`/articles/${articleId}?lang=fr`}>French</Link>
+      </div>
+    </div>
+  );
+}
