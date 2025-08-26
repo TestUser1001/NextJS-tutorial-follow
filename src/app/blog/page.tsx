fb78b5f88af98b9fd39456f1fcd5b6ef198647c4
Inna P <nonamer099@gmail.com>
2025-08-26 13:37:35 +0300

    loading ui

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,15 @@
 import { Metadata } from "next";
+import { setTimeout } from "timers";
 
 export const metadata = {
   title: { absolute: "Blog" },
 };
 
-export default function Blog() {
+export default async function Blog() {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Intentional delay");
+    }, 2000);
+  });
   return <h1>My blog</h1>;
 }
