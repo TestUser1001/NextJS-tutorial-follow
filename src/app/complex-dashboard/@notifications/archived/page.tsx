688b1459fb327fb4d9e127caccee465ecaffa7b5
Inna P <nonamer099@gmail.com>
2025-08-28 18:49:51 +0300

    31 Unmatched routes

--- src/app/complex-dashboard/@notifications/archived/page.tsx
+++ src/app/complex-dashboard/@notifications/archived/page.tsx
@@ -1,16 +1,16 @@
 import { Card } from "@/components/card";
 import Link from "next/link";
 
-export default function Notifications() {
+export default function ArchivedNotifications() {
   return (
     <Card>
-      <div>Notifications</div>
+      <div>Default Notifications</div>
       <div>
         <Link
-          href="/complex-dashboard/archived"
+          href="/complex-dashboard"
           className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
         >
-          Archived
+          Default notifications
         </Link>
       </div>
     </Card>
--- src/app/complex-dashboard/layout.tsx
+++ src/app/complex-dashboard/layout.tsx
@@ -18,7 +18,7 @@ export default function ComplexDashboardLayout({
         <div style={{ display: "flex", flexDirection: "column" }}>
           <div>{users}</div>
           <div>{revenue}</div>
-        </div>{" "}
+        </div>
         <div style={{ display: "flex", flex: 1 }}>{notifications}</div>
       </div>
     </>
